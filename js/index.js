c9cf87de33925dbb1361c39fe437aa0f079e52fd
Xie Ru <xieru@idwzx.com>
2016-09-02 20:17:48 +0800

    test

--- js/index.js
+++ js/index.js
@@ -8,14 +8,12 @@ app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/',{template:'这是首页页面'})
         .when('/onlineWorker',{
-            templateUrl : 'https://sheldon-xieru.github.io/CNCMonitor/#/onlineWorker'
+            templateUrl : 'pages/onlineWorker.html'
         })
         .when('/diary',{
             templateUrl : 'pages/diary.html'
         })
 
         // .otherwise({redirectTo:'/'});
-
-
 
 }]);
