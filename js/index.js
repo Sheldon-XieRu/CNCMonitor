163a1854d60b80acfac234d01109bff24a9716ee
Xie Ru <xieru@idwzx.com>
2016-09-03 11:14:44 +0800

    图表添加

--- js/index.js
+++ js/index.js
@@ -2,13 +2,14 @@
 
 
 
-var app = angular.module('cncMonitor',['ngRoute']);
+var app = angular.module('cncMonitor',['ngRoute','ng-fusioncharts']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/',{template:'这是首页页面'})
         .when('/onlineWorker',{
-            templateUrl : 'pages/onlineWorker.html'
+            templateUrl : 'pages/onlineWorker.html',
+            controller: 'OnlineWorkerController as vm'
         })
         .when('/diary',{
             templateUrl : 'pages/diary.html'
@@ -17,3 +18,102 @@ app.config(['$routeProvider', function($routeProvider){
         // .otherwise({redirectTo:'/'});
 
 }]);
+
+app.controller('OnlineWorkerController',function () {
+    var vm = this;
+
+    vm.firstName = "John";
+    vm.lastName = "Doe";
+
+    vm.myDataSource = {
+        "chart": {
+            "caption": "Monthly revenue for last year",
+            "subCaption": "Harry's SuperMart",
+            "xAxisName": "Month",
+            "yAxisName": "Revenues (In USD)",
+            "numberPrefix": "$",
+            "paletteColors": "#0075c2",
+            "bgColor": "#ffffff",
+            "borderAlpha": "20",
+            "canvasBorderAlpha": "0",
+            "usePlotGradientColor": "0",
+            "plotBorderAlpha": "10",
+            "placevaluesInside": "1",
+            "rotatevalues": "1",
+            "valueFontColor": "#ffffff",
+            "showXAxisLine": "1",
+            "xAxisLineColor": "#999999",
+            "divlineColor": "#999999",
+            "divLineDashed": "1",
+            "showAlternateHGridColor": "0",
+            "subcaptionFontBold": "0",
+            "subcaptionFontSize": "14"
+        },
+        "data": [
+            {
+                "label": "Jan",
+                "value": "420000"
+            },
+            {
+                "label": "Feb",
+                "value": "810000"
+            },
+            {
+                "label": "Mar",
+                "value": "720000"
+            },
+            {
+                "label": "Apr",
+                "value": "550000"
+            },
+            {
+                "label": "May",
+                "value": "910000"
+            },
+            {
+                "label": "Jun",
+                "value": "510000"
+            },
+            {
+                "label": "Jul",
+                "value": "680000"
+            },
+            {
+                "label": "Aug",
+                "value": "620000"
+            },
+            {
+                "label": "Sep",
+                "value": "610000"
+            },
+            {
+                "label": "Oct",
+                "value": "490000"
+            },
+            {
+                "label": "Nov",
+                "value": "900000"
+            },
+            {
+                "label": "Dec",
+                "value": "730000"
+            }
+        ],
+        "trendlines": [
+            {
+                "line": [
+                    {
+                        "startvalue": "700000",
+                        "color": "#1aaf5d",
+                        "valueOnRight": "1",
+                        "displayvalue": "Monthly Target"
+                    }
+                ]
+            }
+        ]
+    };
+
+
+});
+
+
